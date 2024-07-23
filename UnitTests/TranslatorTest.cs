650ea90f791b1f136c87c60154e90ed967e434ae
Lance Kind <lancerkind@yahoo.com>
2024-07-22 22:00:47 -0400

    Added unit test.

--- UnitTests/TranslatorTest.cs
+++ UnitTests/TranslatorTest.cs
@@ -8,6 +8,7 @@ namespace PigLatinBlazor.TranslatorLogic
         public void TranslateWord()
         {
             PigLatinBlazor.TranslatorLogic.Translator translator = new();
+            Assert.Equal("catay", translator.Translate("cat"));
         }
 
         [Fact]
